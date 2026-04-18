2641f113d7acfa890b0dbdfcf39629a575ce80ac
Ordenmoria <a2305103@correo.uia.mx>
2026-04-18 14:03:51 -0600

    Add localhost:5174 to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
         "http://localhost:3000",
         "http://localhost:4173",
     ],
